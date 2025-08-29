ce931bc8e0480af64b4d68bd1bf6226a2042feb5
~ANJALI~ <anjalikumari15243@gmail.com>
2025-08-29 04:46:42 -0500

    Update question 5.py

--- question 5.py
+++ question 5.py
@@ -6,8 +6,13 @@
 *__*
 '''
 
+n = int(input("Enter no. of rows : "))
 
-rows = 5
-for i in range(rows):
-    print('* _ _ _ *')
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if(j==1 or j==5):
+            print('*',end="")
+        else:
+            print(' ',end="")
+    print()
 
