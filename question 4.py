e99b4f531fc41b197c5aef458baf21bb21e343f1
~ANJALI~ <anjalikumari15243@gmail.com>
2025-08-29 04:31:32 -0500

    Update question 4.py

--- question 4.py
+++ question 4.py
@@ -8,12 +8,15 @@
 
 
 
-n=int(input("enter the number:"))
-for i in range(1,n+1):
-     for j in range(1,i+1):
-         if (j%2==0):
-             print("*",end='')
-         else:
-            print(j,end='')
-     print()
+
   
+
+n = int(input("Enter no. of rows : "))
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        if(j%2 == 0):
+            print('*',end=" ")
+        else:
+            print(j,end=" ")   
+    print()
