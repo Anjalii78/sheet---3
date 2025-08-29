6d7d6000f1d6e40130d8fbcad106934a0227249c
~ANJALI~ <anjalikumari15243@gmail.com>
2025-08-29 04:33:58 -0500

    Update question 15.py

--- question 15.py
+++ question 15.py
@@ -8,26 +8,14 @@
 #**
 #*
 
-n = 5  
-
-# Upper part
-for i in range(1, n + 1):
-    for j in range(i):
-        print("*", end="")
+n = 5
+for i in range(0,n+1):
+    for j in range(0,i):
+        print("*", end=" ")
     print()
 
-# Lower part
-for i in range(n - 1, 0, -1):
-    for j in range(i):
-        print("*", end="")
+n = 4
+for i in range(0,n):
+    for j in range(0,n-i):
+        print("*", end=" ")
     print()
-
-
-#combined..:)
-n = 5 
-for i in range(1, n + 1):
-    print('*' * i)
-for i in range(n - 1, 0, -1):
-    print('*' * i)
-
-
