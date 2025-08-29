31350ee1b991dd94ca8c6e210292ddb58137ee0d
~ANJALI~ <anjalikumari15243@gmail.com>
2025-08-28 19:33:56 -0500

    Update question 4.py

--- question 4.py
+++ question 4.py
@@ -1,16 +1,19 @@
-#1
-#1*
-#1*3
-#1*3*
-#1*3*5
+'''
+1
+1*
+1*3
+1*3*
+1*3*5
+'''
 
 
-line = ''
-num = 1
-for i in range(1, 6):
-    if i % 2 == 0:
-        line += '*'
-    else:
-        line += str(num)
-        num += 2
-    print(line)
+
+n=int(input("enter the number:"))
+for i in range(1,n+1):
+     for j in range(1,i+1):
+         if (j%2==0):
+             print("*",end='')
+         else:
+            print(j,end='')
+     print()
+  
