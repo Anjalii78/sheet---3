01be210a598fb1a6dae524ae68796c249c500d8a
~ANJALI~ <anjalikumari15243@gmail.com>
2025-08-28 19:42:40 -0500

    Update question 9.py

--- question 9.py
+++ question 9.py
@@ -5,24 +5,11 @@
 #*        *
 
 
-n = 5  # Number of rows
+n = 5  
+for i in range(n):
+    star = n - i
+    space = i * 2
 
-for i in range(n, 0, -1):
-    stars = i
-    spaces = (n - i) * 2
+    print('*' * star + ' ' * space + '*' * star)
 
-    print("*" * stars + " " * spaces + "*" * stars)
-
-
-
-n=5
-i=1
-#formula used 2i-2
-star=n-1+1
-space=2-i-2
-star=n-1+1
-n=int(input())
-for i in range(1,n+1):
-    for j in range(n-i+1):
-        print("*",end="")
 
