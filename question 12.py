97c99f641d51c039e500a82879ed7e51c6459a92
~ANJALI~ <anjalikumari15243@gmail.com>
2025-08-29 04:48:07 -0500

    Update question 12.py

--- question 12.py
+++ question 12.py
@@ -5,10 +5,12 @@
 #1234
 
 
-n = 4  
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        print(j, end='')
+
+n = int(input("Enter no. of rows : "))
+count = 0
+for i in range(1, n+1): 
+    count = 0
+    for j in range(0, i):
+        count += 1
+        print(count, end=" ")
     print()
-    
-    
