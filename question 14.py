db4c2cecbdaa131b03d983e0861686cc4004f505
~ANJALI~ <anjalikumari15243@gmail.com>
2025-08-28 19:47:16 -0500

    Update question 14.py

--- question 14.py
+++ question 14.py
@@ -3,11 +3,12 @@
 #4 5 6
 #7 8 9 10
 
-n = 4  # number of rows
-num = 1
 
+
+n = 4  
+num = 1
 for i in range(1, n + 1):
     for j in range(i):
-        print(num, end=' ')
-        num += 1
+        print(num, end=" ")
+        num = num + 1
     print()
